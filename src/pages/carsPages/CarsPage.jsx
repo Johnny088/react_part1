6a7578764de109a7c3c5df833eff56e79f65cafb
Johnny <esp25goodlife@gmail.com>
2026-02-16 15:48:08 +0200

    init

---
+++ src/pages/carsPages/CarsPage.jsx
@@ -0,0 +1,28 @@
+import CarCard from './CarCard';
+
+export default CarsPage = () => {
+  return (
+    <Grid container spacing={2} mx={'100px'} my={'50px'}>
+      {authors.map(a => {
+        return (
+          <Grid size={3} key={a.id}>
+            <CarCard item={a} />
+          </Grid>
+        );
+      })}
+      <Box
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        {/* <Link to="/authors/create"> */}
+        <IconButton>
+          <AddReactionIcon sx={{ fontSize: '40px', color: 'green' }} />
+        </IconButton>
+        {/* </Link> */}
+      </Box>
+    </Grid>
+  );
+};
